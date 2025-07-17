69ba94aa7a9f85e19a7205cc2eea4fbc3632e1ca
kathryngraysonnanz <kathrynkaygrayson@gmail.com>
2025-07-17 16:06:24 -0400

    added microbundle support to export customized components to dist

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/* This file is what's referenced when we build our custom component library for dist – make sure you add new components in here! */ 
+
+import './index.css';
+
+export { Button } from './stories/Button';
+export { Card } from './stories/Card'; 
+export { DateTimePicker } from './stories/DateTimePicker'
